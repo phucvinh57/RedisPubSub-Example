994156f5d88ccb1fcd7c600985063c41f239ab20
Nguyễn Phúc Vinh <nguyenphucvinh05072001@gmail.com>
2022-03-22 23:31:07 +0700

    ??

--- subscriber_2/index.js
+++ subscriber_2/index.js
@@ -1,7 +1,7 @@
 const Redis = require('ioredis')
 const fs = require('fs')
 
-const subscriber = new Redis('172.17.0.2')
+const subscriber = new Redis('cache')
 
 subscriber.subscribe('article', (err, count) => {
     if (err) {
